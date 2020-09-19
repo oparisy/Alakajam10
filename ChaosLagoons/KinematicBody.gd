405be7ece369274add6ede9491106123ae96a31a
oparisy <olivier.parisy@gmail.com>
2020-09-19 19:58:53 +0200

    Capping vitesse, amélioration déplacement

--- ChaosLagoons/KinematicBody.gd
+++ ChaosLagoons/KinematicBody.gd
@@ -6,6 +6,9 @@ extends KinematicBody
 
 export var speed = 0.4
 export var rot_speed = 0.85
+export var max_speed = 15
+
+var max_speed_squared = max_speed * max_speed
 
 # This will maintain player velocity
 var velocity = Vector3.ZERO
@@ -20,15 +23,26 @@ func _physics_process(delta):
 
 # See https://www.youtube.com/watch?v=rOA8i_clm1Y
 func get_input(delta):
+	var changed = false
 	if Input.is_action_pressed("forward"):
 		# "Local forward vector"
 		velocity += -transform.basis.z	* speed
+		changed = true
 	if Input.is_action_pressed("backward"):
 		velocity += transform.basis.z	* speed
+		changed = true
 	if Input.is_action_pressed("rotate_left"):
 		rotate_y(rot_speed * delta)
+		changed = true
 	if Input.is_action_pressed("rotate_right"):
 		rotate_y(-rot_speed * delta)
+		changed = true
+
+	if changed:
+		var currentSpeedSqr = velocity.length_squared()
+		if currentSpeedSqr > max_speed_squared:
+			# Cap max speed while keeping direction
+			velocity = velocity.normalized() * max_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
